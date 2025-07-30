fa5601be99f223c8ec8a9fa64f04150205e8041c
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-07-30 12:27:58 +0200

    added map, still need to fix clusters

--- src/components/maps/MapAll.tsx
+++ src/components/maps/MapAll.tsx
@@ -6,7 +6,7 @@ import "@/components/maps/fixLeafletIcons";
 import Image from "next/image";
 const MapAll = ({ campings }: { campings: Camping[] }) => {
   return (
-    <MapContainer>
+    <MapContainer className="w-screen h-100 lg:h-150 md:h-150 rounded-lg" zoom={7} maxZoom={18} center={[50.68072319026102, 5.299799969579857]}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -14,7 +14,16 @@ const MapAll = ({ campings }: { campings: Camping[] }) => {
       {campings.map((camping: Camping) => (
         <Marker key={camping.id} position={[camping.field_camping_map.lat, camping.field_camping_map.lon]}>
           <Popup>
-            <Image src={camping.field_camping_image.uri.url} width={300} height={200} sizes="100%" alt={camping.title} className="w-full h-full" />
+            <Image
+              src={`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${camping.field_camping_image.uri.url}`}
+              width={300}
+              height={200}
+              sizes="100%"
+              alt={camping.title}
+              className="w-full h-full"
+            />
+            <h3 className="mt-2">{camping.title}</h3>
+            <p>{camping.field_camping_location.name}</p>
           </Popup>
         </Marker>
       ))}
