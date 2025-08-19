853297cc9716f280177bae99ddae8a3f8835a303
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-08-19 15:06:37 +0200

    added inputs to show the min/max price

--- src/components/minmaxslider/MinMaxSlider.tsx
+++ src/components/minmaxslider/MinMaxSlider.tsx
@@ -4,8 +4,26 @@ import "rc-slider/assets/index.css";
 
 const MinMaxSlider = () => {
   return (
-    <div className="w-50">
+    <div className="w-50 flex flex-col gap-2">
       <Slider range step={1} defaultValue={[0, 100]} min={0} max={100} />
+      <div className="gap-2 flex">
+        <input
+          type="text"
+          id="minimumPrice"
+          name="minimumPrice"
+          value="0"
+          readOnly
+          className="w-full border border-gray-300 rounded-md px-2 py-1 bg-white"
+        />
+        <input
+          type="text"
+          id="maximumPrice"
+          name="maximumPrice"
+          value="100"
+          readOnly
+          className="w-full border border-gray-300 rounded-md px-2 py-1 bg-white"
+        />
+      </div>
     </div>
   );
 };
